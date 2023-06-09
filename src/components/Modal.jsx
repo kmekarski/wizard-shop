738a71d37165448579c8fb043995dec6e8d9fad2
kmekarski <72306134+kmekarski@users.noreply.github.com>
2023-06-09 16:06:44 +0200

    confirmation modals done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,24 +10,87 @@ export default function Modal(props) {
         modalContext.setVisible(false)
     }
 
+    let title = "Do you want to continue?"
+    let subtitle = "Following action cannot be reversed"
+    let confirmText = "Yes"
+    let cancelText = "No"
+
+    switch(modalContext.content) {
+        case "deleteReview": {
+            title = "Confirm"
+            subtitle = "Are you sure you want to remove this review?"
+            break
+        }
+        case "addProduct": {
+            title = "Confirm"
+            subtitle = "Are you sure you want to add product with provided parameters?"
+            break
+        }
+        case "editProduct": {
+            title = "Confirm"
+            subtitle = "Are you sure you want to edit this product with provided details?"
+            break
+        }
+        case "deleteProduct": {
+            title = "Confirm"
+            subtitle = "Are you sure you want to delete this product?"
+            break
+        }
+        case "addressSubmit": {
+            title = "Confirm"
+            subtitle = "Are you sure provided address is correct?"
+            break
+        }
+        case "paymentSubmit": {
+            title = "Confirm"
+            subtitle = "Are you sure provided data is correct and want to proceed?"
+            break
+        }
+        case "blockUser": {
+            title = "Confirm"
+            subtitle = "Are you sure you want to block this user?"
+            break
+        }
+        case "unblockUser": {
+            title = "Confirm"
+            subtitle = "Are you sure you want to unblock this user?"
+            break
+        }
+        case "completeOrder": {
+            title = "Confirm"
+            subtitle = "Are you sure you want to complete this order?"
+            break
+        }
+        case "deleteOrder": {
+            title = "Confirm"
+            subtitle = "Are you sure you want to delete this order?"
+            break
+        }
+        case "reacceptOrder": {
+            title = "Confirm"
+            subtitle = "Are you sure you want to return this order to the list of pending orders?"
+            break
+        }
+    }
+
     return (
         <div>
             {modalContext.visible && <div className="modal">
                 <div className={`modal__card--${modalContext.size} card--small`}>
                     <div className="modal__header text--medium-bold text--dark">
                         <p className="text--medium-bold text--dark">
-                            Do you want to procceed? {modalContext.content}
+                            {title}
                         </p>
                         <p className="text--small-regular text--dark">
-                            This action cannot be reversed.
+                            {subtitle}
                         </p>
                     </div>
                     <div className="modal__buttons">
                         <div className="btn--solid btn--small btn" onClick={modalContext.handleButtonClick}>
-                            Yes
+                            {confirmText}
                         </div>
                         <div className="btn--solid btn--small btn" onClick={handleNoClick}>
-                            No
+                            {cancelText}
                         </div>
                     </div>
                 </div>

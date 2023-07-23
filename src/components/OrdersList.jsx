f1bb2b09d89b26b22acf7f39e926034c6b7cc792
Filip Sówka <filip@sowka.net>
2023-07-23 17:37:25 +0200

    panel admina - zamówienia, płatności

--- src/components/OrdersList.jsx
+++ src/components/OrdersList.jsx
@@ -16,16 +16,65 @@ export default function OrdersList(props) {
 
     const header = ["Order ID", "Username", "Name", "Date", "Status", "Items", "Action"]
 
-    function completeOrder() {
+    const backendAddr = 'https://wishop.azurewebsites.net/api'
+
+    /*
+    0 - unpaid
+    1 - paid
+    2 - completed (shipped and delivered)
+    3 - rejected
+     */
+    function completeOrder(order) {
         //odpowiedni fetch dla zatwierdzenia zamowienia tutaj
+        order.orderState = 2
+        fetch(backendAddr + '/Order/' + order.id + "?orderSate=2", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        })
+          .then((response) => {
+              if (!response.ok) {
+                  throw new Error("Network response was not ok");
+              }
+              return response;
+          })
+          .then((data) => {
+              console.log("Order completed: ", data);
+          })
+          .catch((error) => {
+              console.error("Error completing order: ", error);
+          });
     }
 
-    function deleteOrder() {
+    function deleteOrder(order) {
         //odpowiedni fetch dla usuniecia zamowienia z historii tutaj
+        order.orderState = 3
+        fetch(backendAddr + '/Order/' + order.id + "?orderSate=3", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        })
+          .then((response) => {
+              if (!response.ok) {
+                  throw new Error("Network response was not ok");
+              }
+              return response;
+          })
+          .then((data) => {
+              console.log("Order rejected: ", data);
+          })
+          .catch((error) => {
+              console.error("Error rejecting order: ", error);
+          });
     }
 
-    function reacceptOrder() {
+    function reacceptOrder(order) {
         //odpowiedni fetch dla ponownego przyjęcia zamowienia tutaj
+        completeOrder(order)
     }
 
     function showItemsList() {
@@ -37,24 +86,24 @@ export default function OrdersList(props) {
         modalContext.setList(items)
     }
 
-    function handleActionClick(e) {
+    function handleActionClick(e, order) {
         e.preventDefault()
         switch (e.target.innerHTML) {
             case "Complete": {
-                modalContext.setCallback(completeOrder)
+                modalContext.setCallback(() => completeOrder(order))
                 break;
             }
             case "Delete": {
-                modalContext.setCallback(deleteOrder)
+                modalContext.setCallback(() => deleteOrder(order))
                 break;
             }
             case "Reaccept": {
-                modalContext.setCallback(reacceptOrder)
+                modalContext.setCallback(() => reacceptOrder(order))
                 break;
             }
         }
     }
-
+/*
     const pendingOrders = [
         {
             id: "AKN15208",
@@ -139,21 +188,52 @@ export default function OrdersList(props) {
             status: "rejected"
         }
     ]
+*/
+
+    const [orders, setOrders] = useState([])
+
+    const getOrders = async (e) => {
+        const response = await fetch(backendAddr + "/Order", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            },
+        })
+          .then((response) => {
+              if (!response.ok) {
+                  throw new Error("Network response was not ok");
+              }
+              return response.json();
+          })
+          .then((data) => {
+              setOrders(data)
+          })
+          .catch((error) => {
+              console.error("Error getting reviews:", error);
+          })
+    };
+
+    useEffect(() => {
+        getOrders()
+          .catch(error => {
+              console.error(error);
+          });
+    }, []);
 
 
-
-    const usersHtml = (props.type === "pending" ? pendingOrders : props.type === "completed" ? completedOrders : rejectedOrders).map((el, index) => {
+    const usersHtml = (props.type === "pending" ? orders.filter(order => order.orderState === 0 || order.orderState === 1) : props.type === "completed" ? orders.filter(order => order.orderState === 2) : orders.filter(order => order.orderState === 3)).map((el, index) => {
         return (
             <div className={`orders-list__row ${index % 2 === 1 ? "orders-list__row--gray" : ""}`}>
                 <p className="text--small-regular text--dark">{el.id}</p>
-                <p className="text--small-regular text--dark">{el.username}</p>
-                <p className="text--small-reguler text--dark">{el.name}</p>
-                <p className="text--small-regular text--dark">{el.date}</p>
-                <p className="text--small-regular text--dark">{el.status}</p>
-                <div className="btn--solid btn--small btn" onClick={() => handleShowClick(el.items)}>
+                <p className="text--small-regular text--dark">{el.email}</p>
+                <p className="text--small-reguler text--dark">{el.firstName + " " + el.lastName}</p>
+                <p className="text--small-regular text--dark">{el.dateCreated}</p>
+                <p className="text--small-regular text--dark">{el.orderState === 0 ? "unpaid" : el.orderState === 1 ? "paid" : el.orderState === 2 ? "completed" : el.orderState === 3 ? "rejected" : "other"}</p>
+                <div className="btn--solid btn--small btn" onClick={() => handleShowClick(el.orderItems)}>
                     Show
                 </div>
-                <div className="btn--solid btn--small btn" onClick={handleActionClick}>
+                <div className="btn--solid btn--small btn" onClick={(e) => handleActionClick(e, el)}>
                     {props.type === "pending" ? "Complete" : props.type === "completed" ? "Delete" : "Reaccept"}
                 </div>
             </div>
